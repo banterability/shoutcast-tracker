e5fc198807f9b1fec5fbd5e171598e15c4987c11
KPCC: Dev 1 <dev1@scpr.org>
2009-09-30 17:31:47 -0700

    Initial Commit

---
+++ app.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+require 'date'
+require 'net/http'
+require 'uri'
+
+url = URI.parse("http://live.scpr.org/")
+request = Net::HTTP::Get.new(url.path)
+request.add_field("User-Agent", "Mozilla/4.0 (StatICE/1.0)") #need recognizable UA string since stream is on port 80
+
+response = Net::HTTP.new(url.host, url.port).start do |http|
+  http.request(request)
+end
+
+txt = response.body
+
+re='with <B>?([0-9]*) of'
+
+m=Regexp.new(re,Regexp::IGNORECASE);
+if m.match(txt)
+    file = File.new("stats.csv", "a")
+    file.puts("#{m.match(txt)[1]}, #{DateTime.now.strftime("%m/%d/%Y %H:%M:%S")}")
+    file.close
+end
